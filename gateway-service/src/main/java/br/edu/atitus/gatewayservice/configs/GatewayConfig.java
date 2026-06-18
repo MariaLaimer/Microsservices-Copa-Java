0a3f95109d5ea61a1629a064758c73181ac4042b
Leo981-web <leozancanaro@outlook.com.br>
2026-06-17 22:04:59 -0300

    Voltando com a validção inicial

--- gateway-service/src/main/java/br/edu/atitus/gatewayservice/configs/GatewayConfig.java
+++ gateway-service/src/main/java/br/edu/atitus/gatewayservice/configs/GatewayConfig.java
@@ -4,9 +4,6 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsWebFilter;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class GatewayConfig {
@@ -22,27 +19,5 @@ public class GatewayConfig {
                 .route(p -> p.path("/auth/**").uri("lb://auth-service"))
                 .route(p -> p.path("/ws/orders/**").uri("lb://order-service"))
                 .build();
-    }
-
-
-    @Bean
-    public CorsWebFilter corsWebFilter() {
-        CorsConfiguration corsConfig = new CorsConfiguration();
-        
-       
-        corsConfig.addAllowedOriginPattern("*"); 
-        
-        
-        corsConfig.addAllowedMethod("*"); 
-        
-        
-        corsConfig.addAllowedHeader("*"); 
-        
-        corsConfig.setAllowCredentials(true);
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", corsConfig);
-
-        return new CorsWebFilter(source);
     }
 }
